b9beaff89c01472a0fde5e271acccf578a9158ba
MauSon1971 <110358138+MauSon1971@users.noreply.github.com>
2024-10-04 23:13:10 +0000

    2cmt - Desafío 2 terminado

--- src/app.js
+++ src/app.js
@@ -7,35 +7,54 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = ["com", "net", "es", "us"];
+
   let listaResultado = document.getElementById("lista1");
+  let listaResultado2 = document.getElementById("lista2");
+
+  // Genero los eventos de los botones
   document.getElementById("clearBtn").addEventListener("click", function() {
     listaResultado.innerHTML = "";
+    listaResultado2.innerHTML = "";
   });
-  document
-    .getElementById("actionBtn1")
-    .addEventListener("click", generaDomName);
+  document.getElementById("actionBtn1").addEventListener("click", function() {
+    generaDomName(pronoun, adj, noun);
+  });
+
+  document.getElementById("actionBtn2").addEventListener("click", function() {
+    generaDomName2(pronoun, adj, noun, dom);
+  });
 
   //Genero nombres de dominio
-  function generaDomName() {
-    let pronoun = ["the", "our"];
-    let adj = ["great", "big"];
-    let noun = ["jogger", "racoon"];
-
+  function generaDomName(pronoun, adj, noun) {
     listaResultado.innerHTML = "";
 
     for (let i = 0; i < pronoun.length; i++) {
-      // let nombreDominio = pronoun[i] + ".com";
-      // listaResultado.innerHTML += "<p>" + nombreDominio + "</p";
-
       for (let j = 0; j < adj.length; j++) {
-        // let nombreDominio = pronoun[i] + adj[j] + ".com";
-        // listaResultado.innerHTML += "<p>" + nombreDominio + "</p";
-
-        // console.log(pronoun[i] + adj[j]);
         for (let k = 0; k < noun.length; k++) {
           let nombreDominio = pronoun[i] + adj[j] + noun[k] + ".com";
-          listaResultado.innerHTML += "<p>" + nombreDominio + "</p";
-          // console.log(pronoun[i] + adj[j] + noun[k]);
+          listaResultado.innerHTML += "<p>" + nombreDominio + "</p>";
+        }
+      }
+    }
+  }
+
+  //Genero nombres de dominio
+  function generaDomName2(pronoun, adj, noun, dom) {
+    listaResultado2.innerHTML = "";
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < dom.length; l++) {
+            let nombreDominio = pronoun[i] + adj[j] + noun[k] + "." + dom[l];
+            console.log(nombreDominio);
+            listaResultado2.innerHTML += "<p>" + nombreDominio + "</p>";
+          }
         }
       }
     }
